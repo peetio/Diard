e8992ff17d565a7eba49a8457d9559a8ea6b473a
thibaultvt <thibault.vantricht@hotmail.com>
2022-05-20 19:42:37 +0200

    update readme.md and main.py example

---
+++ docs/examples.md
@@ -0,0 +1,24 @@
+# Table of Contents
+- [Examples Introduction](#examples-introduction)
+- [x](#x)
+
+## Examples Introduction
+In this document all the pipeline's functionalities are explained with code examples. If the main example didn't fulfil your document analysis wishes, then hopefully this guide will help you with that.
+
+## Processing A Single Document (PDF)
+Processing a single document is probably be the most common use case. That's why we decided to use this as the main example (main.py). We recommend you to first take a look at the [main.py](../main.py) file to get an idea of the workflow because example below builds from this source code.
+
+```bash
+
+```
+
+
+
+## Processing Multiple Documents (PDF)
+## Using Images Instead of Documents
+## Exporting JSON / HTML For All Document Pages 
+## Exporting JSON / HTML Of Specific Document Page
+## Loading Layouts From JSON
+## Output Directory Structure
+## Enabling Table Extraction
+TODO: make sure you have installed the table-transformer pre-trained weights
--- main.py
+++ main.py
@@ -17,12 +17,12 @@ def main():
         "ignore", category=UserWarning
     )  # NOTE: comment out for debugging
 
-    config_path = "./resources/model_configs/cascade/cascade_dit_large.yaml"
-    weights_path = "./resources/weights/publaynet_dit-l_cascade.pth"
+    ld_config_path = "./resources/model_configs/cascade/cascade_dit_large.yaml"
+    ld_weights_path = "./resources/weights/publaynet_dit-l_cascade.pth"
 
     ld = LayoutDetection(
-        cfg_path=config_path,
-        weights_path=weights_path,
+        cfg_path=ld_config_path,
+        weights_path=ld_weights_path,
         device='cuda',
         batch_size=1,
         workers=1,
@@ -33,9 +33,6 @@ def main():
     metadata = ld.get_metadata()
     source_dir = "./resources/pdfs/"
 
-    table_weights_path = "./resources/weights/pubtables1m_structure_detr_r18.pth"
-    table_predictor = TableExtractor(table_weights_path, device='cpu')
-
     lang = "deu"  #   language used most of your documents
     langs = ["eng", "fra", "deu"]  #   only useful if lang_detect=True
 
@@ -43,24 +40,6 @@ def main():
     # filenames = ["example.pdf"]
 
     #   process multiple pdfs
-    doc_path = "resources/doc_images"
-    doc = Document(
-        doc_path,
-        predictor=predictor,
-        metadata=metadata,
-        table_predictor=table_predictor,
-        lang=lang,
-        lang_detect=True,
-        langs=langs,
-        use_images=True
-    )
-
-    doc.extract_layouts(visualize=True, segment_sections=True)
-    doc.order_layouts()
-    doc.save_layouts_as_json()
-    doc.save_layouts_as_html()
-
-    """
     filenames = os.listdir(source_dir)
 
     for filename in filenames:
@@ -70,19 +49,17 @@ def main():
             doc_path,
             predictor=predictor,
             metadata=metadata,
-            table_predictor=table_predictor,
             lang=lang,
             lang_detect=True,
             langs=langs,
         )
 
-        #   extract & save layout
+        #   extract & export layouts
         doc.doc_to_images()
         doc.extract_layouts(visualize=True, segment_sections=True)
         doc.order_layouts()
         doc.save_layouts_as_json()
         doc.save_layouts_as_html()
-    """
 
 if __name__ == "__main__":
     main()
